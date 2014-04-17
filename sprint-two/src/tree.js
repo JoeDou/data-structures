7924ed3b00a48304e24c339dff31e9c6da7bc3b2
Hack Reactor Students <joe.dou@gmail.com>
2014-04-16 22:42:09 -0700

    Fix errors in tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,19 @@
+var _ = {};
+
+_.extend = function(obj) {
+    for(var i = 1; i < arguments.length; i++){
+      for(var j in arguments[i]){
+        obj[j] = arguments[i][j];
+      }
+    }
+    return obj;
+  };
+
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree,treeMethods);
   return newTree;
 };
 
@@ -9,8 +21,26 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  if (this.children === undefined) {
+    this.children = [];
+  }
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  if (this.children === undefined) {
+    if (target === this.value) {
+      return true;
+    }
+  } else {
+    for (var i=0; i<this.children.length; i++){
+      if(this.children[i].contains(target)){
+        return true;
+      }
+    }
+  }
+  return false
 };
 
+/// var tree = makeTree(10);
+// tree.addChild(5)
