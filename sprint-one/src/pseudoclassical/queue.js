be2155edba33d46bd88e7256d976869ebe30cd8e
Hack Reactor Students <joe.dou@gmail.com>
2014-04-16 18:24:05 -0700

    Fix psudeoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,21 +3,19 @@ var Queue = function(){
   this.index = 0;
 };
 
-Queue.prototype = {
-  enqueue: function(value){
-    this.storage[this.index] = value;
-    this.index++;
-  },
-  dequeue: function(){
-    this.index && this.index--;
-    var results = this.storage[0];
-    delete this.storage[0];
-    for (var i=1; i < this.index+1; i++){
-      this.storage[i-1] = this.storage[i];
-    }
-    return results;
-  },
-  size: function(){
-    return this.index;
+Queue.prototype.enqueue = function(value){
+  this.storage[this.index] = value;
+  this.index++;
+};
+Queue.prototype.dequeue = function(){
+  this.index && this.index--;
+  var results = this.storage[0];
+  delete this.storage[0];
+  for (var i=1; i < this.index+1; i++){
+    this.storage[i-1] = this.storage[i];
   }
+  return results;
+};
+Queue.prototype.size = function(){
+  return this.index;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,20 +3,16 @@ var Stack = function() {
   this.index = 0;
 };
 
-Stack.prototype = {
-  push: function(value){
-    this.storage[this.index] = value;
-    this.index++;
-  },
-  pop: function(){
-    this.index && this.index--;
-    var result = this.storage[this.index];
-    delete this.storage[this.index];
-    return result;
-  },
-  size: function(){
-    return this.index;
-  }
+Stack.prototype.push = function(value){
+  this.storage[this.index] = value;
+  this.index++;
 };
-
-var stack = new Stack();
+Stack.prototype.pop = function(){
+  this.index && this.index--;
+  var result = this.storage[this.index];
+  delete this.storage[this.index];
+  return result;
+};
+Stack.prototype.size = function(){
+  return this.index;
+};
