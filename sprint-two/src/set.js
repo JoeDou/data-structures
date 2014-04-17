25267ebb10d376b9b49a66d5b6ec948c495d9ca0
Hack Reactor Students <joe.dou@gmail.com>
2014-04-16 23:16:05 -0700

    Fix errors in set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,24 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (this._storage === undefined){
+    this._storage = {};
+  }
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage[item]){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if (this._storage[item]){
+    delete this._storage[item];
+  }
 };
+
+// set1 = makeSet()
+// set2 = makeSet()
